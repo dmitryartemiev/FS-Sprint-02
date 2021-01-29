bee46dbf4e80e36d3979d9e83b4ed9f488a0f5fc
dmitryartemiev <dima77261@gmail.com>
2021-01-29 11:17:54 +0200

    t00 is done (alert)

---
+++ t00/js/script.js
@@ -0,0 +1,4 @@
+'use strict'
+alert('Hello JavaScript from inside!')
+// An alert box is often used if you want to make sure information comes through to the user.
+// When an alert box pops up, the user will have to click "OK" to proceed.
